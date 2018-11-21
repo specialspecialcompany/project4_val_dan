7a1c39353ea5572a30c7600158da087bbdfe4546
DanDitomaso <dan.ditomaso@gmail.com>
2018-11-20 23:15:47 -0500

    Added inital Map functionality to project.

---
+++ dev/js/app.js
@@ -0,0 +1,27 @@
+// Create app namespace to hold all methods
+const maps = {};
+maps.googleApiKey = 'AIzaSyCQXH5sS5BqaSv72Nbb3AJnx4yzjWCSbtE'
+
+// Collect user input
+maps.getZomatoData = function () {
+
+}
+
+// Make AJAX request with user inputted data
+maps.getInfo = function () {
+
+}
+
+// Display data on the page
+maps.displayMap = function () {
+
+}
+
+// Start app
+maps.init = function () {
+
+}
+
+$(function () {
+    maps.init();
+});
